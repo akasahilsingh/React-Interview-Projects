6d43e1ce1fed5fa88c2b2faae044d2e027598cc7
Sahil Singh <akasahilsingh@gmail.com>
2025-10-12 22:31:52 +0530

    feat: created custom hook useLocalStorage

---
+++ Light and Dark Mode/src/useLocalStorage.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+
+const useLocalStorage = (key, theme) => {
+
+    const [value, setValue] = useState(()=> {
+        let currentTheme;
+
+        try {
+        currentTheme = JSON.parse(localStorage.getItem(key) || String(theme))
+        } catch(e) {
+            console.log(e);
+            currentTheme = theme;
+        }
+
+        return currentTheme;
+    })
+
+    useEffect(()=> {
+    localStorage.setItem(key, JSON.stringify(value))
+    }, [key, theme])
+
+    
+  return [value, setValue]
+}
+
+export default useLocalStorage
